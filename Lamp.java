a96867f0594eaa8920cc7eb64bad673478e06c12
sridhar <svarala@seas.upenn.edu>
2020-03-28 05:18:59 -0400

    createe a initial

---
+++ Lamp.java
@@ -0,0 +1,39 @@
+package test5;
+
+public class Lamp {
+	
+	private boolean isOn = false;
+	
+	public Lamp(boolean isOn) {
+		
+		this.isOn =isOn;
+		int x;
+	}
+	
+	public void turnOnLamp() {
+		
+		
+		this.isOn =true;
+	}
+	
+	
+	public static void main(String args[]) {
+		
+		
+		Lamp []lamps = new Lamp[4];
+		
+		
+		
+		for ( int i =0 ; i < lamps.length ; i++) {
+			
+			lamps[i] = new Lamp(false);
+			
+		System.out.println(lamps[i].isOn);	
+		
+		lamps[i].turnOnLamp();
+		
+		System.out.println(lamps[i].isOn);	
+	
+}
+}
+}
